e29e2cfbf5e60bcdc274fbfb1af0c2f05756a88d
nils <26379736+NiGour@users.noreply.github.com>
2023-04-23 07:29:46 +0200

    created a grid with numbers

---
+++ calculator.js
@@ -0,0 +1,16 @@
+console.log("test");
+
+let test=document.querySelector(".contenu");
+let nouveau=document.createElement("div");
+nouveau.innerText=""
+nouveau.innerText+=""
+
+
+
+
+
+
+//test.appendChild(nouveau);
+
+
+
