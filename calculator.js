41de025db4472d43a8f4e6e19170b7f9606c31b7
nils <26379736+NiGour@users.noreply.github.com>
2023-04-24 11:43:38 +0200

    the keypad allows to display digits on the calculator screen

--- calculator.js
+++ calculator.js
@@ -5,12 +5,51 @@ let nouveau=document.createElement("div");
 nouveau.innerText=""
 nouveau.innerText+=""
 
+window.addEventListener('keydown',function(e){
+    const digit=document.querySelector(`div[data-key="${e.key}"]`);
+    console.log(e);
+    digit.classList.add('pressed');
+    
+    document.querySelector('.screen').textContent+=digit.innerHTML;
+    
+});
+window.addEventListener('keyup',function(e){
+    const digit=document.querySelector(`div[data-key="${e.key}"]`);
+    console.log(e);
+    digit.classList.remove('pressed');
+    
+    
+});
 
+function add(numberOne, numberTwo){
+    return numberOne+numberTwo;
+}
+function multiply(numberOne, numberTwo){
+    return numberOne+numberTwo;
+}
+function substract(numberOne, numberTwo){
+    return numberOne+numberTwo;
+}
+function divide(numberOne, numberTwo){
+    return numberOne+numberTwo;
+}
+function power(numberOne, numberTwo){
+    return Math.pow(numberOne,numberTwo)
+}
 
-
-
-
-//test.appendChild(nouveau);
-
+function operate(numberOne, operator, numberTwo){
+    switch (operator){
+        case '*':
+        return multiply(numberOne,numberTwo);
+        case '*':
+        return divide(numberOne,numberTwo);
+        case '*':
+        return add(numberOne,numberTwo);    
+        case '*':
+        return substract(numberOne,numberTwo);   
+        default:
+        return ("invalid")
+    }
+}
 
 
