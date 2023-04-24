d78d9484a1a45ec2b465c4012c9fe0f49c128d5b
nils <26379736+NiGour@users.noreply.github.com>
2023-04-24 14:22:16 +0200

    added a screen to store the previous input

--- calculator.js
+++ calculator.js
@@ -1,21 +1,56 @@
+
+
+const availableOperators=['+','-','*','/'];
+const temp=document.querySelector('.temp');
+const stored=document.querySelector('.stored')
+let numberOne='';
+let numberTwo='';
+let operator=''
+
 console.log("test");
 
-let test=document.querySelector(".contenu");
-let nouveau=document.createElement("div");
-nouveau.innerText=""
-nouveau.innerText+=""
+
+console.log(operate(numberOne,operator,numberTwo));
+
 
 window.addEventListener('keydown',function(e){
     const digit=document.querySelector(`div[data-key="${e.key}"]`);
-    console.log(e);
+    
+    //console.log(e);
     digit.classList.add('pressed');
     
-    document.querySelector('.screen').textContent+=digit.innerHTML;
+    
+    
+    if(digit.innerHTML=="="){
+        temp.textContent=operate(numberOne,operator,numberTwo);
+    }
+    
+    if(!availableOperators.includes(digit.innerHTML)){
+        if(digit.innerHTML!="="){
+            temp.textContent+=digit.innerHTML;
+        }
+    }
+    
+    
+    
+    if(availableOperators.includes(digit.innerHTML)){
+        if(operator==''){
+            operator=digit.innerHTML;
+            numberOne=temp.textContent;
+            stored.textContent=numberOne+operator;
+            temp.textContent="";
+            
+            
+        }else{
+            numberTwo=temp.textContent;
+            temp.textContent=operate(numberOne,operator,numberTwo);
+        }
+        
+    }
     
 });
 window.addEventListener('keyup',function(e){
     const digit=document.querySelector(`div[data-key="${e.key}"]`);
-    console.log(e);
     digit.classList.remove('pressed');
     
     
@@ -25,27 +60,27 @@ function add(numberOne, numberTwo){
     return numberOne+numberTwo;
 }
 function multiply(numberOne, numberTwo){
-    return numberOne+numberTwo;
+    return numberOne*numberTwo;
 }
 function substract(numberOne, numberTwo){
-    return numberOne+numberTwo;
+    return numberOne-numberTwo;
 }
 function divide(numberOne, numberTwo){
-    return numberOne+numberTwo;
+    return numberOne/numberTwo;
 }
 function power(numberOne, numberTwo){
     return Math.pow(numberOne,numberTwo)
 }
 
-function operate(numberOne, operator, numberTwo){
+function operate(numberOne,operator,numberTwo){
     switch (operator){
         case '*':
         return multiply(numberOne,numberTwo);
-        case '*':
+        case '/':
         return divide(numberOne,numberTwo);
-        case '*':
+        case '+':
         return add(numberOne,numberTwo);    
-        case '*':
+        case '-':
         return substract(numberOne,numberTwo);   
         default:
         return ("invalid")
